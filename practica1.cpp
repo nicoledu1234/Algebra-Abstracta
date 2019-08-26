2570521e8ba4b9ff6bf5eb3d2e5b1f3928d76474
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-08-26 15:15:08 -0500

    Update practica1.cpp

--- practica1.cpp
+++ practica1.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main()
 {
-   //string alfabeto={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','0','1','2','3','4','5','6','7','8','9',' '};
     string alfabeto="abcdefghijklmnopqrstuvwxyz0123456789 ";
     cout<<"Ingresar las letras por teclado"<<endl;
     string mensaje;
