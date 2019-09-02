ec7735cba2891bdfb7dd316313db008191615aa8
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-09-02 18:25:04 -0500

    Update cifrado.cpp

--- cifrado.cpp
+++ cifrado.cpp
@@ -5,65 +5,73 @@
 using namespace std;
 class Vigenere{
     private:
-        string clave="";
+        string clave;
     public:
         string alfabeto="abcdefghijklmnopqrstuvwxyz0123456789 ";
-        string mensaje="";
         int tam;
-        Vigenere(string mensaje){
-            this->mensaje=mensaje;
-            tam=mensaje.length();
-        }
-        Vigenere(string mensaje,string clave){
-            this->mensaje=mensaje;
-            tam=mensaje.length();
-            this->clave=clave;
-        }
-        void cifrado(){
+        Vigenere(int tam){
+            this->tam=tam;
             srand(time(NULL));
             int i, aleatorio, DESDE=0, HASTA=alfabeto.length();
             for (i = 0; i < tam; i ++){
                 aleatorio = rand()%(HASTA-DESDE+1)+DESDE;
-                clave.push_back(alfabeto.at(aleatorio));
+                clave+=(alfabeto.at(aleatorio));
             }
             cout<< "clave "<<clave<<endl;
+        }
+        Vigenere(string clave){
+            this->tam=clave.length();
+            this->clave=clave;
+        }
+        string cifrado(string mensaje){
             this->clave=clave;
             for (int i=0 ; i< tam;i++){
                 int m=alfabeto.find(mensaje.at(i));
                 int c=alfabeto.find(clave.at(i));
-                int sumaT=(m+c)%37;
+                int sumaT=(m+c)%alfabeto.length();
                 mensaje.at(i)=alfabeto.at(sumaT);
             }
             cout<<"mensaje "<<mensaje<<endl;
+            return mensaje;
         }
-        void descifrado(){
+        string descifrado(string mensaje){
             tam=mensaje.length();
+
             for (int i=0 ; i< tam;i++){
                 int m=alfabeto.find(mensaje.at(i));
                 int c=alfabeto.find(clave.at(i));
-                int restaT=(m-c)%37;
+                cout<<alfabeto.length()<<endl;
+                int restaT=(m-c);
+                cout<<alfabeto.length()<<endl;
                 if(restaT<0){
-                    restaT = (37 + restaT)%37;
+                    restaT = (alfabeto.length() + restaT)%alfabeto.length();
                 }
                 mensaje.at(i)=alfabeto.at(restaT);
             }
             cout<<"mensaje "<<mensaje<<endl;
+            return mensaje;
         }
 
 };
 int main()
 {
-    cout<<"Ingresar las letras por teclado"<<endl;
-    string mensaje;
+
+   /* cout<<"Ingresar las letras por teclado"<<endl;
+   string mensaje;
     getline(cin,mensaje);
-    Vigenere a(mensaje);
-    a.cifrado();
-    /*
-    string men="at";
-    string k="nf";
-    Vigenere b(men,k);
-    b.descifrado();
-    */
+    Vigenere a(mensaje.length());
+    a.cifrado(mensaje);
+*/
+
+
+     cout<<"Ingresar el texto cifrado"<<endl;
+     string men;
+     string k;
+    getline(cin,men);
+    cout<<"Ingresar la clave"<<endl;
+    getline(cin,k);
+    Vigenere b(k);
+    b.descifrado(men);
 
     return 0;
 }
