cb3f4b7af2d489e5ea1d9f39adde06ff30b16231
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-09-08 16:49:35 -0500

    Update main.cpp

--- Escitala/main.cpp
+++ Escitala/main.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
+#include "Escitala.h"
+#include <string>
 
 using namespace std;
 
 int main()
 {
     cout<<"Ingresar las letras por teclado"<<endl;
-   string mensaje;
+    string mensaje;
     getline(cin,mensaje);
     cout<<"Ingresar la clave por filas y columnas"<<endl;
-   int filas;
-   int columnas;
-   cin>>filas;
-   cin>>columnas;
-   cout<<endl;
-   cout<<filas<<endl;
-   cout<<columnas<<endl;
-   if((filas*columnas)>mensaje.length()){
-       for(int i=mensaje.length();i<filas*columnas; i++){
-            mensaje.push_back(' ');
-       }
-   }
-   string cifrado;
-   for(int i=0;i<columnas;i++){
-       int t=0;
-     for(int j=i;j<filas+i;j++,t=t+columnas){
-            cifrado+=mensaje.at(i+t);
-       }
-   }
-   cout<<endl;
-   cout<<cifrado<<endl;
-   string descifrado;
-    for(int i=0;i<filas;i++){
-            int t=0;
-     for(int j=i;j<columnas+i;j++,t=t+filas){
-            descifrado+=cifrado.at(i+t);
-       }
-   }
-   cout<<descifrado<<endl;
+    int filas;
+    int columnas;
+    cin>>filas;
+    cin>>columnas;
+    Escitala emisor(filas,columnas,mensaje.length());
+    string meme= emisor.cifrado(mensaje);
+
+    Escitala reseptor(filas,columnas,mensaje.length());
+    reseptor.descifrado(meme);
 
     return 0;
 }
