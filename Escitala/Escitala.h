6278d93903a419103111b99efad00dd5e9c6f42b
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-09-08 20:56:42 -0500

    Add files via upload

---
+++ Escitala/Escitala.h
@@ -0,0 +1,21 @@
+#ifndef ESCITALA_H
+#define ESCITALA_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Escitala
+{
+    private:
+        int filas;
+        int columnas;
+    public:
+        int tam;
+        Escitala(int filas, int columnas, int tam);
+        Escitala(int filas, int columnas);
+        string cifrado(string mensaje);
+        string descifrado(string mensaje);
+        virtual ~Escitala();
+};
+
+#endif // ESCITALA_H
