f6f96508a64aa012a48ab88cb4348f26bf5d0902
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-09-23 14:40:15 -0500

    Update main.cpp

--- Escitala/main.cpp
+++ Escitala/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Escitala.h"
 #include <string>
 
@@ -6,19 +7,41 @@ using namespace std;
 
 int main()
 {
-    cout<<"Ingresar las letras por teclado"<<endl;
+    ifstream entrada ("textoPlano.txt");
+    ofstream salida ("textoCifrado.txt");
     string mensaje;
-    getline(cin,mensaje);
-    cout<<"Ingresar la clave por filas y columnas"<<endl;
+    while (getline (entrada,mensaje)) {
+    }
+    cout<<"Ingresar la clave, filas y columnas"<<endl;
     int filas;
     int columnas;
     cin>>filas;
     cin>>columnas;
-    Escitala emisor(filas,columnas,mensaje.length());
+    Escitala emisor(filas,columnas);
     string meme= emisor.cifrado(mensaje);
+    salida << meme << endl;
+    salida.close();
+    entrada.close();
 
-    Escitala reseptor(filas,columnas,mensaje.length());
-    reseptor.descifrado(meme);
+
+    cout<<"Ingresar la clave, filas y columnas"<<endl;
+    int filas1;
+    int columnas1;
+    cin>>filas1;
+    cin>>columnas1;
+    Escitala reseptor(filas1,columnas1);
+    ifstream entrada1 ("textoCifrado.txt");
+    ofstream salida1 ("textoDescifrado.txt");
+    string mensaje1;
+    while (getline (entrada1,mensaje1)) {
+    }
+
+    string mensj=reseptor.descifrado(meme);
+    salida1 << mensj<< endl;
+    cout<<mensj<<endl;
+    salida1.close();
+    entrada1.close();
+
 
     return 0;
 }
