ddd84bd5857e689327e02e8be68712343c21627c
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-09-23 14:38:57 -0500

    Update Vernam.h

--- Vernam/Vernam.h
+++ Vernam/Vernam.h
@@ -2,26 +2,31 @@
 #define VERNAM_H
 #include <iostream>
 #include <ctime>
+#include <iostream>
+#include <ctime>
 #include <bitset>
+#include <vector>
 #include <cstdlib>
 #include <string>
 #include <math.h>
+#include <cstring>
+#include <stdlib.h>
+#include "Vernam.h"
 using namespace std;
-
 class Vernam
 {
     private:
         string clave;
-        string claveNum[37];
+        //string claveNum[string];
     public:
         string alfabeto="abcdefghijklmnopqrstuvwxyz0123456789 ";
         int tamAlfabeto=alfabeto.length();
         int tam;
-        string mensajeNum[37];
+        //string mensajeNum[37];
         Vernam(int tam);
         Vernam(string clave);
-        string cifrado(string mensaje);
-        string descifrado(string mensaje);
+        vector<string> cifrado(string mensaje);
+        string descifrado(vector<string> mensaje);
         virtual ~Vernam();
 
     protected:
