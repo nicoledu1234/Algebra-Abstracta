42e72bbaf13130590098cf646fb9e062b5307f1f
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-09-26 19:45:51 -0500

    Update main.cpp

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -1,18 +1,190 @@
-#include <iostream>
-#include <string>
 #include "Enigma.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
 
+int mod(int t, int m) {
+  int mo = -1;
+  if (t < 0)
+	  return (t*mo >= m) ? (t - (t/m * m)) + m : t+ m;
+  return (t >= m) ? t - (t/m * m) : t;
+}
 
-
-
-
-int main()
+Enigma::Enigma(string clave)
 {
- /*Enigma a("cvr");
- a.cifrado("hola");*/
- Enigma b("cvr","abefqtuvxhisnkjgdwzpcoym lr","ahpqrvw dxjbyonklfuzegmsict","bcefhlmoquwsatx pdirkvynjzg");
- b.descifrado("aiwg")
-    return 0;
+    string alf="abcdefghijklmnopqrstuvwxyz ";
+    string alf1="abcdefghijklmnopqrstuvwxyz ";
+    string alf2="abcdefghijklmnopqrstuvwxyz ";
+    string alfabetoAleatorio;
+    string alfabetoAleatorio1;
+    string alfabetoAleatorio2;
+    srand(time(NULL));
+    int i, aleatorio,aleatorio1,aleatorio2, DESDE=0, HASTA=alf.length()-1;
+    for (i = 0; i < alf.length(); i ++){
+        aleatorio = mod(rand(),(HASTA-DESDE)+DESDE);
+        alf+=alf.at(aleatorio);
+        alf.erase(aleatorio,1);
+        alfabetoAleatorio=alf;
+        aleatorio1 = mod(rand(),(HASTA-DESDE)+DESDE);
+        alf1+=alf1.at(aleatorio1);
+        alf1.erase(aleatorio1,1);
+        alfabetoAleatorio1=alf1;
+        aleatorio2 = mod(rand(),(HASTA-DESDE)+DESDE);
+        alf2+=alf2.at(aleatorio2);
+        alf2.erase(aleatorio2,1);
+        alfabetoAleatorio2=alf2;
+    }
+
+     r1Rojo=alfabetoAleatorio;
+     r2Rojo=alfabetoAleatorio1;
+     r3Rojo=alfabetoAleatorio2;
+     cout<<"r1Rojo  "<<r1Rojo<<endl;
+     cout<<"r2Rojo  "<<r2Rojo<<endl;
+     cout<<"r3Rojo  "<<r3Rojo<<endl;
+
+
+     r1Azul+=r1Azul.substr(0,r1Azul.find(clave.at(2)));
+    r1Azul.erase(0,r1Azul.find(clave.at(2)));
+
+    cout<<"r1Azul  "<<r1Azul<<endl;
+
+
+    r2Azul+=r2Azul.substr(0,r2Azul.find(clave.at(1)));
+    r2Azul.erase(0,r2Azul.find(clave.at(1)));
+
+    cout<<"r2Azul  "<<r2Azul<<endl;
+
+    r3Azul+=r3Azul.substr(0,r3Azul.find(clave.at(0)));
+    r3Azul.erase(0,r3Azul.find(clave.at(0)));
+
+    cout<<"r3Azul  "<<r3Azul<<endl;
+
+}
+
+Enigma::Enigma(string clave,string r1Rojo,string r2Rojo,string r3Rojo)
+{
+    this->r1Rojo+=r1Rojo;
+    this->r2Rojo+=r2Rojo;
+    this->r3Rojo+=r3Rojo;
+
+
+    cout<<"r1Rojo  "<<r1Rojo<<endl;
+     cout<<"r2Rojo  "<<r2Rojo<<endl;
+     cout<<"r3Rojo  "<<r3Rojo<<endl;
+
+
+     r1Azul+=r1Azul.substr(0,r1Azul.find(clave.at(2)));
+    r1Azul.erase(0,r1Azul.find(clave.at(2)));
+
+    cout<<"r1Azul  "<<r1Azul<<endl;
+
+
+    r2Azul+=r2Azul.substr(0,r2Azul.find(clave.at(1)));
+    r2Azul.erase(0,r2Azul.find(clave.at(1)));
+
+    cout<<"r2Azul  "<<r2Azul<<endl;
+
+    r3Azul+=r3Azul.substr(0,r3Azul.find(clave.at(0)));
+    r3Azul.erase(0,r3Azul.find(clave.at(0)));
+
+    cout<<"r3Azul  "<<r3Azul<<endl;
+}
+string  Enigma::cifrado(string mensaje){
+    string cifrado;
+        for(int i=0; i<mensaje.length();i++){
+        cifrado+=r3Rojo.at(r3Azul.find(r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i)))))));
+  /*      cout<<endl;
+        cout<<r1Azul<<endl;
+        cout<<r1Rojo<<endl;
+        cout<<r2Azul<<endl;
+        cout<<r2Rojo<<endl;
+        cout<<r3Azul<<endl;
+        cout<<r3Rojo<<endl;
+
+        cout<<r1Azul.find(mensaje.at(i))<<endl;
+        cout<<r1Rojo.at(r1Azul.find(mensaje.at(i)))<<endl;
+        cout<<r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i))))<<endl;
+        cout<<r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i)))))<<endl;
+        cout<<r3Azul.find(r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i))))))<<endl;
+        cout<<r3Rojo.at(r3Azul.find(r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i)))))))<<endl;
+        cout<<endl;
+*/
+        if((i>5)&&(i<9)){
+            r1Azul+=r1Azul.at(0);
+            r1Azul.erase(0,1);
+            //cout<<"1-4"<<endl;
+        }
+        if((i<13)&&(i>9)){
+            r1Azul+=r1Azul.at(0);
+            r1Azul.erase(0,1);
+            r2Azul+=r2Azul.at(0);
+            r1Azul.erase(0,1);
+            //cout<<"4-8"<<endl;
+        }
+        if(i>13){
+            r1Azul+=r1Azul.at(0);
+            r1Azul.erase(0,1);
+            r2Azul+=r2Azul.at(0);
+            r2Azul.erase(0,1);
+            r2Azul+=r2Azul.at(0);
+            r2Azul.erase(0,1);
+        }
+        cout<<cifrado<<endl;
+    }
+    return cifrado;
+}
+
+string  Enigma::descifrado(string mensaje){
+    string descifrado;
+        for(int i=0;i<mensaje.length();i++){
+        descifrado+=r1Azul.at(r1Rojo.find(r2Azul.at(r2Rojo.find(r3Azul.at(r3Rojo.find(mensaje.at(i)))))));
+        /*cout<<endl;
+        cout<<r1Azul<<endl;
+        cout<<r1Rojo<<endl;
+        cout<<r2Azul<<endl;
+        cout<<r2Rojo<<endl;
+        cout<<r3Azul<<endl;
+        cout<<r3Rojo<<endl;
+
+        cout<<r1Azul.find(mensaje.at(i))<<endl;
+        cout<<r1Rojo.at(r1Azul.find(mensaje.at(i)))<<endl;
+        cout<<r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i))))<<endl;
+        cout<<r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i)))))<<endl;
+        cout<<r3Azul.find(r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i))))))<<endl;
+        cout<<r3Rojo.at(r3Azul.find(r2Rojo.at(r2Azul.find(r1Rojo.at(r1Azul.find(mensaje.at(i)))))))<<endl;
+        cout<<endl;
+*/
+        if((i>5)&&(i<9)){
+            r1Azul+=r1Azul.at(0);
+            r1Azul.erase(0,1);
+           // cout<<"1-4"<<endl;
+        }
+        if((i<13)&&(i>9)){
+            r1Azul+=r1Azul.at(0);
+            r1Azul.erase(0,1);
+            r2Azul+=r2Azul.at(0);
+            r1Azul.erase(0,1);
+            //cout<<"4-8"<<endl;
+        }
+        if(i>13){
+            r1Azul+=r1Azul.at(0);
+            r1Azul.erase(0,1);
+            r2Azul+=r2Azul.at(0);
+            r2Azul.erase(0,1);
+            r2Azul+=r2Azul.at(0);
+            r2Azul.erase(0,1);
+        }
+        cout<<descifrado<<endl;
+    }
+    cout<<descifrado<<endl;
+    return descifrado;
+}
+Enigma::~Enigma()
+{
+    //dtor
 }
