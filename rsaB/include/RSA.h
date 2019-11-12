9f566fd36c3cd8987d30a4a7ee6ecf345a4c8bfb
nicoledu1234 <42581259+nicoledu1234@users.noreply.github.com>
2019-11-11 20:26:50 -0500

    Update RSA.h

--- rsaB/include/RSA.h
+++ rsaB/include/RSA.h
@@ -7,15 +7,13 @@ using namespace NTL;
 class RSA {
   private:
     ZZ e, n, p, q, phiN, d, dp, dq, qCp, pCq;
-    std::string alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ012345679-";
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345679 ";
   public:
     RSA(int);
     virtual ~RSA(){}
     std::string encrypt(std::string);
     std::string decrypt(std::string);
     ZZ chinese_theorem(ZZ);
-    ZZ fast_exponentiation(ZZ, ZZ, ZZ);
-    ZZ gen_rand(int);
 };
 
 #endif // RSA_H
